b46852a564946cd38ae979eb251cb4ee1221b603
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-01-04 14:00:32 +0000

    Edit button wired up & data bind to edit button

--- app.py
+++ app.py
@@ -136,6 +136,14 @@ def write_review():
     return render_template("write_review.html", genre=genre)
 
 
+@app.route("/edit_review/<review_id>", methods=["GET", "POST"])
+def edit_review(review_id):
+    review = mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
+
+    genre = mongo.db.genre.find().sort("genre_name", 1)
+    return render_template("edit_review.html", review=review, genre=genre)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")), 
