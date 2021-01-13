5bdf6cddd4a988b51aeced7c1da4cd2a461ddc50
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-01-13 11:56:34 +0000

    Styling applied accross the site so all pages are consistent with previous commits (Header & Reviews page)

--- static/js/review.js
+++ static/js/review.js
@@ -1,5 +1,8 @@
-//Confirm if user wants to delete review - confirmation is hidden on page launch
+//Confirm if user wants to delete review - confirmation is hidden on page launch - on Review page
 $("#sure").hide();
+
+$("#review-delete-check").hide();
+
 
 
 $('#delete').on('click', function() {
@@ -8,4 +11,15 @@ $('#delete').on('click', function() {
 
 $('#no').on('click', function() {
         $("#sure").hide();
+    });
+
+// Same as above, confirms if user wants to delete review, this time on profile page
+
+
+$('#delete-2').on('click', function() {
+        $("#sure-2").show();
+    });
+
+$('#no-2').on('click', function() {
+        $("#sure-2").hide();
     });
