34eff67fb07283a47902a22adaef5b81e4beaf33
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-01-15 12:10:21 +0000

    Debug mode turned off

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ def comment(review_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
